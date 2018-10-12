9057182962ad3d9a311b9e8ba7474006709c4223
petard <petard2008@gmail.com>
2018-10-12 16:20:01 +0200

    remove debug output

--- Source/PagedArray.swift
+++ Source/PagedArray.swift
@@ -167,7 +167,7 @@ extension PagedArray : BidirectionalCollection {
             
             let elementIndex = position%pageSize
             
-            print("subscript", pageIndex, position%pageSize, elementIndex, elements[pageIndex])
+            //print("subscript", pageIndex, position%pageSize, elementIndex, elements[pageIndex])
 
             if let page = elements[pageIndex], elementIndex < page.count {
                 return page[elementIndex]
